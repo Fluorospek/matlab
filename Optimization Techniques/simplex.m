0ed7b521d7dba055c08c79990632a243cd941456
Tanmay Deshkar <tdeshkar_be21@gamil.com>
2023-04-23 17:42:45 +0530

    Commit

--- Optimization Techniques/simplex.m
+++ Optimization Techniques/simplex.m
@@ -8,7 +8,7 @@ bv=[3 4 5];
 var={'x1','x2','s1','s2','s3','b'};
 ZjCj=cost(bv)*A-cost;
 simplex_table=[A;ZjCj];
-array2table(simplex_table,'VariableNames',var);
+array2table(simplex_table,'VariableNames',var)
 RUN=true;
 while RUN
     ZC=ZjCj(1:end-1);
@@ -28,18 +28,18 @@ while RUN
                 end
             end
         end
-    [Leaving_value Pivot_row]=min(ratio);
-    Pivot_key=A(Pivot_row,Pivot_col);
-    bv(Pivot_row)=Pivot_col;
-    A(Pivot_row,:)=A(Pivot_row,:)/Pivot_key;
-    for i=1:size(A,1)
-        if i~=Pivot_row
-            A(i,:)=A(i,:)-A(i,Pivot_col)*A(Pivot_row,:);
+        [Leaving_value Pivot_row]=min(ratio);
+        Pivot_key=A(Pivot_row,Pivot_col);
+        bv(Pivot_row)=Pivot_col;
+        A(Pivot_row,:)=A(Pivot_row,:)/Pivot_key;
+        for i=1:size(A,1)
+            if i~=Pivot_row
+                A(i,:)=A(i,:)-A(i,Pivot_col)*A(Pivot_row,:);
+            end
         end
-    end
-    ZjCj=cost(bv)*A-cost;
-    new_table=[A;ZjCj];
-    array2table(new_table,'VariableNames',var);
+        ZjCj=cost(bv)*A-cost;
+        new_table=[A;ZjCj];
+        array2table(new_table,'VariableNames',var)
     else
         RUN=false;
     end
